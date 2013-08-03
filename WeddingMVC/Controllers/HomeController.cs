4b6556762ef1288b5cab779e1e7cfce0f05dd08f
dmak563 <dmak563@gmail.com>
2013-08-03 19:05:54 +0300

    First

---
+++ WeddingMVC/Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Web.Mvc;
+using WeddingMVC.Models;
+
+namespace WeddingMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Start()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MassageToUser()
+        {
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            return View();
+        }
+
+        public ActionResult Quiz()
+        {
+            ViewData["listOfIsExist"] = new [] {
+                                            new SelectListItem {Text = "Yes", Value = "True"},
+                                            new SelectListItem {Text = "Yes Yes", Value = "False"}
+                                        };
+            ViewData["listOfSize"] = Enumerable.Range(25, 24)
+                                               .Select(age => Convert.ToString(age, CultureInfo.InvariantCulture))
+                                               .Select(age => new SelectListItem {Text = age, Value = age})
+                                               .ToList();
+            return View(new Quiz());
+        }
+
+        [HttpPost]
+        public ActionResult Quiz(Quiz quiz)
+        {
+            if (IsStupidBot())
+            {
+                TempData["Thanks"] = "You are the bot!";
+            }
+            else
+            {
+                var isSent = SendQuizResultToEmail(quiz);
+                TempData["Thanks"] = isSent ? "Спасибки, всем чмоки!" : "Простите, но результаты не сохранились, пожалуйста сообщите об этом Денису или Алёне. :(";
+            }
+
+            return RedirectToAction("MassageToUser");
+        }
+
+        public ActionResult Email(Quiz quiz)
+        {
+            return View(quiz);
+        }
+
+
+        private bool IsStupidBot()
+        {
+            return !string.IsNullOrEmpty(Convert.ToString(this.Request.Form["honeyspot"]));
+        }
+
+        private bool SendQuizResultToEmail(Quiz quiz)
+        {
+            try
+            {
+                var smtp = new SmtpClient();
+                var mail = new MailMessage
+                {
+                    Subject = "[QUIZ] " + quiz.Fio,
+                    IsBodyHtml = true, Body = GetEmailBody(quiz)
+                };
+                mail.To.Add("dmak563@gmail.com");
+                mail.To.Add("alyonabazhanova@gmail.com");
+				mail.To.Add("andreybazhanov@rambler.ru");
+                smtp.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        private string GetEmailBody(Quiz quiz)
+        {
+            using (var writer = new StringWriter())
+            {
+                var viewData = new ViewDataDictionary { Model = quiz };
+                var tempData = new TempDataDictionary();
+                var view = ViewEngines.Engines.FindView(ControllerContext, "Email", null);
+                var context = new ViewContext(ControllerContext, view.View, viewData, tempData, writer);
+                view.View.Render(context, writer);
+                writer.Flush();
+                return writer.ToString();
+            }
+        }
+    }
+}
---
+++ WeddingMVC/Models/Quiz.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeddingMVC.Models
+{
+    public class Quiz
+    {
+        public Quiz()
+        {
+            IsExist = true;
+            PopcornIsCool = true;
+            FootSize = 41;
+        }
+
+        [MaxLength(50)]
+        [DisplayName("Ваше ФИО")]
+        public string Fio { get; set; }
+
+        [DisplayName("Сможете ли вы прийти на нашу свадьбу?")]
+        public bool IsExist { get; set; }
+
+        [MaxLength(20)]
+        [DisplayName("Ваш любимый цвет")]
+        public string FavouriteColor { get; set; }
+
+        [MaxLength(30)]
+        [DisplayName("Любимая песня или исполнитель вашей молодости(детства)")]
+        public string FavouriteSong { get; set; }
+
+        [MaxLength(5)]
+        [DisplayName("Ваша группа крови")]
+        public string Blood { get; set; }
+
+        [DisplayName("Любите ли вы попкорн?")]
+        public bool PopcornIsCool { get; set; }
+
+        [DisplayName("Ваш размер обуви")]
+        public int FootSize { get; set; }
+
+        [MaxLength(250)] 
+        [DisplayName("Почему кактус колючий?")]
+        public string WhyIsCactusPrickly { get; set; }
+
+        [MaxLength(50)]
+        [DisplayName("Желтый или зеленый?")]
+        public string YellowOrGreen { get; set; }
+        
+        [MaxLength(50)]
+        [DisplayName("Столица Бразилии?")]
+        public string CapitalOfBrazil { get; set; }
+        
+        [MaxLength(5000)]
+        [DisplayName("Несколько слов о нас")]
+        public string AboutUs { get; set; }
+
+        [MaxLength(5000)]
+        [DisplayName("Смешной случай или воспоминания связанные с женихом/невестой")]
+        public string Joke { get; set; }
+    }
+}
